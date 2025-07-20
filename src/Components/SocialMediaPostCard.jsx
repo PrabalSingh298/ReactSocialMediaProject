703dfd76887b3c2187d2e8c6a37f851d75c2f028
Prabal Singh <prabal.29082002@gmail.com>
2025-07-20 23:58:26 +0530

    Make fetching posts from aerver API automatically

--- src/Components/SocialMediaPostCard.jsx
+++ src/Components/SocialMediaPostCard.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { postListContext } from "../Store/HomeContext";
 import Post from "./Post";
 import WelcomeMessage from "./WelcomeMessage";
@@ -7,20 +7,24 @@ const SocialMediaPostCard = () => {
 
     const postList = useContext(postListContext);
     const postListArray = postList.postlist;
-    const handleFetchPost = () => {
+
+    const [initialPost, setInitialPost] = useState(false);
+
+    if (!initialPost) {
         fetch('https://dummyjson.com/posts')
             .then(res => res.json())
             .then(jsonObject => postList.fetchPost(
                 jsonObject.posts));
-
+        setInitialPost(true);
     }
+
 
 
 
 
     return (<div className="PostContainer">
 
-        {(postListArray.length === 0) && <WelcomeMessage handleFetchPost={handleFetchPost}></WelcomeMessage>}
+        {(postListArray.length === 0) && <WelcomeMessage></WelcomeMessage>}
         {postListArray.map((item) => (<Post postObject={item} key={item.id}>
         </Post>))}
     </div>
--- src/Components/WelcomeMessage.jsx
+++ src/Components/WelcomeMessage.jsx
@@ -6,7 +6,6 @@ const WelcomeMessage = (props) => {
     return <div className="welcome_message">
         <center>
             <h1>There are no posts.</h1>
-            <button type="button" class="btn btn-primary" onClick={props.handleFetchPost}>Fetch Posts from Server.</button>
         </center>
     </div>
 }
